f4e072e4fc344a70d017ec27d320db2b8030ba36
paniz-cu <panizchavoshi3@gmail.com>
2025-03-26 17:17:05 +0330

    Add files via upload

---
+++ dist/Brands.js
@@ -0,0 +1,20 @@
+async function Barnds() {
+    try {
+        let data = await fetch("http://localhost:3001/brands");
+        let res = await data.json();
+        // ✅ لیست اصلی را دو بار پشت سر هم قرار می‌دهیم
+        let sliderItems = res.map((item) => `
+            <div class="slideb slider-item  rounded-xl shadow-md p-10 "
+                style="background-image: url('${item.image}');">
+            </div>
+            
+        `);
+        let sliderContent = [...sliderItems, ...sliderItems].join("");
+        // مقداردهی به `.slider2`
+        document.querySelector(".brands").innerHTML = sliderContent;
+    }
+    catch (error) {
+        console.error("Error fetching slider data:", error.message);
+    }
+}
+export default Barnds;
